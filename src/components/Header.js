6b08bbc008c7a00ee9fabd8a929f49251a8c03e6
Abhinaba Roy <75931274+Abhinaba12@users.noreply.github.com>
2022-09-05 10:54:09 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+
+    return (
+        <div className="header">
+            <h1>To-do List</h1>
+        </div>
+    );
+};
+
+export default Header;
